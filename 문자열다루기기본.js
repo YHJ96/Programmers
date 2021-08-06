3ccbb26b121a6ea7bdb46e87bdd3fb3235b2a88c
YHJ96 <yhj878812@naver.com>
2021-08-06 19:23:20 +0900

    YHJ96

--- 문자열다루기기본.js
+++ 문자열다루기기본.js
@@ -1,12 +1,11 @@
-//테스트케이스 5,6번 에러
-
-const s = "1234";
+const s = "1245";
 
 function solution(s) {
     let answer = [...s];
     answer = answer.map(item => parseInt(item));
     answer = answer.reduce((acc, curr) => acc + curr);
-    if(isNaN(answer) && (s.length !== 4 || s.length !==6)) return false;
+    if(isNaN(answer)) return false;
+    if(s.length !== 4 && s.length !==6) return false;
     else return true;
 }
 
