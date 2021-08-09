27f8ab2575e72bed41b6fce9bf69a7506f2d8556
YHJ96 <yhj878812@naver.com>
2021-08-10 01:55:32 +0900

    YHJ96

---
+++ 상호평가.js
@@ -0,0 +1,15 @@
+//푸는중
+
+const scores = [[100,90,98,88,65],[50,45,99,85,77],[47,88,95,80,67],[61,57,100,80,65],[24,90,94,75,65]];
+
+function solution(scores) {
+    let answer = [];
+    for(let i = 0; i < scores.length; i++) {
+        if(scores[i][i] === Math.max(...scores[i]) || scores[i][i] === Math.min(...scores[i])) {
+            answer.push(scores[i][i]);
+        }
+    }
+    return answer;
+}
+
+console.log(solution(scores));
