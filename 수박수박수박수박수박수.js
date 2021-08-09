0773f869b07d246b9301d1ee326c64a95e0ea4e3
YHJ96 <yhj878812@naver.com>
2021-08-09 10:11:51 +0900

    YHJ96

---
+++ 수박수박수박수박수박수.js
@@ -0,0 +1,13 @@
+const n = 3;
+
+function solution(n) {
+    let answer = '';
+    for(let i = 1; i <= n; i++) {
+        if(i % 2 !== 0) {
+            answer += '수';
+        } else answer += '박';
+    }
+    return answer;
+}
+
+console.log(solution(n));
