7d89075f450ae84dd63791cbbaa935de8dde7acf
YHJ96 <yhj878812@naver.com>
2021-08-10 02:32:43 +0900

    YHJ96

--- 상호평가.js
+++ 상호평가.js
@@ -4,9 +4,24 @@ const scores = [[100,90,98,88,65],[50,45,99,85,77],[47,88,95,80,67],[61,57,100,8
 
 function solution(scores) {
     let answer = [];
-    for(let i = 0; i < scores.length; i++) {
-        if(scores[i][i] === Math.max(...scores[i]) || scores[i][i] === Math.min(...scores[i])) {
-            answer.push(scores[i][i]);
+    let newArr = [];
+    for(let a = 0; a < scores.length; a++) {
+        newArr.push([]);
+    }
+    for(let b = 0; b < scores.length; b++) {
+        for(let c = 0; c < scores.length; c++) {
+            newArr[b][c] = scores[c][b];
+        }
+    }
+    for(let d = 0; d < scores.length; d++) {
+        if(scores[d][d] === Math.max(...newArr[d]) || scores[d][d] === Math.min(...newArr[d])) {
+           const add = newArr[d].reduce((acc, curr) => acc + curr) - scores[d][d];
+           const avg = add / (scores[d].length - 1)
+           answer.push(avg);
+        } else {
+            const add = newArr[d].reduce((acc, curr) => acc + curr);
+            const avg = add / scores[d].length;
+            answer.push(avg);
         }
     }
     return answer;
