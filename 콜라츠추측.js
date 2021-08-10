2ee11d684f89fe091de0ccc371b37bf855d61b0c
YHJ96 <yhj878812@naver.com>
2021-08-11 01:53:40 +0900

    YHJ96

---
+++ 콜라츠추측.js
@@ -0,0 +1,18 @@
+const num = 626331;
+
+function solution(num) {
+    let answer = 0;
+    while(num !== 1) {
+        if(answer >= 500) return -1;
+        if(num % 2 === 0) {
+            num /= 2;
+            answer += 1;
+        } else {
+            num = num * 3 + 1;
+            answer += 1;
+        }
+    }
+    return answer;
+}
+
+console.log(solution(num));
