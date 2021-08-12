80d7327aa6d67774582edae15e690f4c89f29869
YHJ96 <yhj878812@naver.com>
2021-08-13 01:38:49 +0900

    YHJ96

--- 시저암호.js
+++ 시저암호.js
@@ -1,21 +1,28 @@
-const s = "AB c";
-const n = 1;
+const s = "ab Z";
+const n = 100;
 
 function solution(s, n) {
     const compare = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']; 
     const newStr = [...s];
-    const answer = [];
+    let answer = [];
     for(let i = 0; i < s.length; i++) {
         if(newStr[i] === ' ') {
             answer.push(' ');
         } else if (newStr[i] === newStr[i].toLowerCase()) {
-            const index = compare.indexOf(newStr[i]);
-            answer.push(compare[index + n]);
+            let index = compare.indexOf(newStr[i]) + n;
+            while(index > 25) {
+                index = index - 26;
+            }
+            answer.push(compare[index]);
         } else {
-            const index = compare.indexOf(newStr[i].toLowerCase());
-            answer.push(compare[index + n].toUpperCase());
+            let index = compare.indexOf(newStr[i].toLowerCase()) + n;
+            while(index > 25) {
+                index = index - 26;
+            }
+            answer.push(compare[index].toUpperCase());
         }
     }
+    answer = answer.reduce((acc, curr) => acc + curr);
     return answer;
 }
 
