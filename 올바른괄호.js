0575d37cb1bb1e8478036dfda986b6102aa1c2bd
YHJ96 <yhj878812@naver.com>
2021-08-12 15:19:42 +0900

    YHJ96

--- 올바른괄호.js
+++ 올바른괄호.js
@@ -4,12 +4,16 @@ function solution(s){
     let newArr = [...s];
     let answer = [];
     if(newArr[0] === ')') return false;
+    // 첫 시작이 ')' 일경우 무조건 성립 실패 예외처리
     for(let i = 0; i < newArr.length; i++) {
         if(newArr[i] === '(') {
             answer.push('X');
+            // 첫시작이 무조건 '(' 성립 answer에 값 넣기
         } else answer.pop();
+        // ')'만나면 pop(뺴기)
     }
     if(answer.length > 0) return false;
+    // 올바른 괄호이면 배열에 값이 남을수 없음
     return true;
 }
 
