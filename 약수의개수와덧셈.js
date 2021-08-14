94a969f92107211e65c03c457e1da660525619b3
YHJ96 <yhj878812@naver.com>
2021-08-14 13:44:36 +0900

    YHJ96

--- 약수의개수와덧셈.js
+++ 약수의개수와덧셈.js
@@ -6,10 +6,21 @@ const right = 17;
 function solution(left, right) {
     const degree = right - left;
     const degreeArr = [];
+    const count = [];
     for(let i = 0; i <= degree; i++) {
         degreeArr.push(left + i);
     }
-    return degreeArr;
+    for(let j = 0; j < degreeArr.length; j++) {
+        let num = 0;
+        for(let k = 1; k <= degreeArr[j]; k++ ) {
+            if(degreeArr[j] % j === 0) {
+                num++;
+            }
+            count.push(num);
+            num *= 0;
+        }
+    }
+    return count;
 }
 
 console.log(solution(left, right));
