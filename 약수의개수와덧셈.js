8af026b9ae49c3c6b423086a3487b227168de4c9
YHJ96 <yhj878812@naver.com>
2021-08-15 12:43:31 +0900

    YHJ96

--- 약수의개수와덧셈.js
+++ 약수의개수와덧셈.js
@@ -5,22 +5,31 @@ const right = 17;
 
 function solution(left, right) {
     const degree = right - left;
-    const degreeArr = [];
-    const count = [];
+    let degreeArr = [];
+    const divisorNum = [];
+    let divisorBool = [];
     for(let i = 0; i <= degree; i++) {
         degreeArr.push(left + i);
     }
     for(let j = 0; j < degreeArr.length; j++) {
         let num = 0;
-        for(let k = 1; k <= degreeArr[j]; k++ ) {
-            if(degreeArr[j] % j === 0) {
-                num++;
+        for(let k = 1; k <= degreeArr[j]; k++) {
+            if(degreeArr[j] % k === 0) {
+                num += 1;
             }
-            count.push(num);
-            num *= 0;
         }
+        divisorNum.push(num);
     }
-    return count;
+    divisorBool = divisorNum.map((item) => {
+        if(item % 2 === 0) return true;
+        else return false;
+    })
+    degreeArr = degreeArr.map((item, index) => {
+        if(divisorBool[index] === true) return item;
+        if(divisorBool[index] === false) return -item;
+    })
+    const answer = degreeArr.reduce((acc, curr) => acc + curr);
+    return answer;
 }
 
 console.log(solution(left, right));
