453e0d6cf6b3fd974fe7580a2cf0a239eeb9530d
YHJ96 <yhj878812@naver.com>
2021-08-20 17:54:33 +0900

    YHJ96

--- 체육복.js
+++ 체육복.js
@@ -1,5 +1,5 @@
 // 푸는중
-
+// 테스트케이스 2,4번 오류
 /*
 n = 학생수
 lost = 도난당한 학생수
@@ -11,22 +11,24 @@ const lost = [2, 4];
 const reserve = [3];
 
 function solution(n, lost, reserve) {
-    let newLost = lost.filter((item) => !reserve.includes(item));
-    let newReserve = reserve.filter((item) => !lost.includes(item));
+    let newLost = lost.filter((item) => !reserve.includes(item)).sort((a,b) => a-b);
+    let newReserve = reserve.filter((item) => !lost.includes(item)).sort((a,b) => a-b);
     let minus = [];
     for(let i = 0; i < newLost.length; i++) {
         for(let j = 0; j < newReserve.length; j++) {
             if(newLost[i] - newReserve[j] === -1 || newLost[i] - newReserve[j] === 1) {
                 minus.push(newLost[i]);
-                newLost.splice(newLost[i], 1);
-                newReserve.splice(newReserve[j], 1);
+                delete newLost[i];
+                delete newReserve[i];
+                // newLost.splice(i, 1);
+                // newReserve.splice(j, 1);
             }
         }
     }
-    console.log('newLost : ', newLost, 'newReserve :', newReserve);
+    console.log('newLost :', newLost, 'newReserve :', newReserve)
     newLost = newLost.filter((item) => !minus.includes(item));
     const answer = n - newLost.length;
-    return minus;
+    return answer;
 }
 
 console.log(solution(n, lost, reserve));
