8bbf9f767d4915a174c11210cece23cee194c72e
YHJ96 <yhj878812@naver.com>
2021-08-22 08:15:19 +0900

    YHJ96

---
+++ 최대공약수와최소공배수.js
@@ -0,0 +1,23 @@
+const n = 3;
+const m = 12;
+
+function solution(n, m) {
+    let n1 = n;
+    let m1 = m;
+    let remainder = 0;
+    let max = 0;
+    let min = 0;
+    for(;;) {
+      remainder = n1 % m1;
+      n1 = m1;
+      m1 = remainder;
+      if(remainder === 0) {
+        max = n1;
+        break
+      }
+    }
+    min = (n * m) / max;
+    return [max, min];
+}
+
+console.log(solution(n, m));
