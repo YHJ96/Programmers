dedffeb4bd2d12fde8c3002b9ae5b2c39a2bd07b
YHJ96 <yhj878812@naver.com>
2021-08-21 09:16:19 +0900

    YHJ96

--- 3진법뒤집기.js
+++ 3진법뒤집기.js
@@ -4,7 +4,7 @@ function solution(n) {
     let ter = n.toString(3);
     ter = [...ter].reverse();
     ter = ter.join('');
-    const answer = parseInt(ter, 10);
+    const answer = parseInt(ter, 3);
     return answer;
 }
 
