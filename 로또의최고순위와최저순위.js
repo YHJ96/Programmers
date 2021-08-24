19881465691b0de1cbf117d2a32ab0ab994b7c08
YHJ96 <yhj878812@naver.com>
2021-08-24 11:38:49 +0900

    YHJ96

--- 로또의최고순위와최저순위.js
+++ 로또의최고순위와최저순위.js
@@ -1,5 +1,3 @@
-// 푸는중
-
 const lottos = [44, 1, 0, 0, 31, 25];
 const win_nums = [31, 10, 45, 1, 6, 19];
 
@@ -9,6 +7,14 @@ function solution(lottos, win_nums) {
     newWin_nums = newWin_nums.filter((item) => newLottos.includes(item));
     newLottos = newLottos.filter((item) => item === 0);
     let answer = [newLottos.length + newWin_nums.length , newWin_nums.length];
+    answer = answer.map((item) => {
+        if(item === 6) return 1;
+        if(item === 5) return 2;
+        if(item === 4) return 3;
+        if(item === 3) return 4;
+        if(item === 2) return 5;
+        return 6;
+    });
     return answer;
 }
 
