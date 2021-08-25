756e6095b2dbf02246333cfc115a55da96f7d4cc
YHJ96 <yhj878812@naver.com>
2021-08-25 12:04:07 +0900

    YHJ96

---
+++ 신규아이디추천.js
@@ -0,0 +1,11 @@
+// 푸는중
+
+const new_id = 	"...!@BaT#*..y.abcdefghijklm";
+
+function solution(new_id) {
+    let answer = new_id;
+    answer = answer.toLowerCase();
+    return answer;
+}
+
+console.log(solution(new_id));
