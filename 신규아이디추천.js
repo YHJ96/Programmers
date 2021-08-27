b880a3280197298aa5b3e772e08fa408cecd6c28
YHJ96 <yhj878812@naver.com>
2021-08-27 22:29:59 +0900

    YHJ96

--- 신규아이디추천.js
+++ 신규아이디추천.js
@@ -6,22 +6,29 @@ function solution(new_id) {
     'o','p','q','r','s','t','u','v','w','x','y','z'
     ,'_','-','.','1','2','3','4','5','6','7','8','9','0'];
     answer = answer.toLowerCase();
+    // 1단계
     answer = [...answer];
     answer = answer.filter((item) => level2.includes(item));
+    // 2단계
     for(let i = 0; i < answer.length; i++) {
         if(answer[i] === '.' && answer[i + 1] === '.') {
             delete answer[i];
         } continue;
     }
     answer = answer.filter((item) => answer.includes(item));
+    // 3단계
     if(answer[0] === '.') answer.shift();
     if(answer[answer.length - 1] === '.') answer.pop();
+    // 4단계
     if(answer.length === 0) answer.push('a');
+    // 5단계
     if(answer.length >= 16) answer = answer.slice(0, 15);
     if(answer[answer.length - 1] === '.') answer.pop();
+    // 6단계
     while(answer.length < 3) {
         answer.push(answer[answer.length - 1]);
     }
+    // 7단계
     answer = answer.join('');
     return answer;
 }
