60cd7ebecdd8106baf36358a1e7cd40eb3960222
YHJ96 <yhj878812@naver.com>
2021-08-29 17:30:38 +0900

    YHJ96

--- 키패드누르기.js
+++ 키패드누르기.js
@@ -6,10 +6,12 @@ const hand = "right";
 function solution(numbers, hand) {
     const pad = [ [1, 2, 3], [4, 5, 6], [7, 8, 9], ['*', 0, '#'] ];
     const xyAxis = pad.map((item) => {
-        const yAxis = pad.map((yValue, index) => {
+        const yAxis = pad
+        .map((yValue, index) => {
             if(yValue.includes(item)) return 4 - index;
             return -1;
         })
+        .filter((item) => item !== -1);
         return yAxis;
     })
     return xyAxis;
