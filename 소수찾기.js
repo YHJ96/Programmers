8b5ca82e6d199ddcc842d7dd0104c118c7f0ae12
YHJ96 <yhj878812@naver.com>
2021-09-01 14:29:02 +0900

    YHJ96

--- 소수찾기.js
+++ 소수찾기.js
@@ -1,10 +1,20 @@
 // 에라토스테네스의 체 사용
-// 푸는중
-
 const n = 10;
 
 function solution(n) {
-    var answer = 0;
+    let answer = [0, 0];
+    for(let i = 2; i <= n; i++) {
+        answer.push(i);
+    }
+
+    for(let j = 2; j <= n; j++) {
+        if(answer[j] === 0) continue;
+        for(let k = j * 2; k <= n; k += j) {
+            answer[k] = 0;
+        }
+    }
+    answer = answer.filter((item) => item !== 0);
+    answer = answer.length;
     return answer;
 }
 
