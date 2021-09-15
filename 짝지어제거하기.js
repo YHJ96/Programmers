b164511f5ed6172e4bda3ca634cf78b58e1ecdda
YHJ96 <yhj878812@naver.com>
2021-09-15 16:57:49 +0900

    YHJ96

--- 짝지어제거하기.js
+++ 짝지어제거하기.js
@@ -1,21 +1,28 @@
 const s = 'baabaa';
 
 // 푸는중
-// 조건식 찾기
+// 테스트케이스 실패
 function solution(s) {
     let arr = [...s];
-    let count = 0;
-    while(count !== 20) {
+    let count = [...arr].length;
+    do {
         for(let i = 0; i < arr.length - 1; i++) {
             if(arr[i] === arr[i+1]) {
                 arr.splice(i + 1, 1);
                 arr.splice(i, 1);
-                compare = arr.length;
             }
         }
-        count++;
+        count = arr.length;
+    }
+    while(arr.length !== count) {
+        for(let i = 0; i < arr.length - 1; i++) {
+            if(arr[i] === arr[i+1]) {
+                arr.splice(i + 1, 1);
+                arr.splice(i, 1);
+            }
+        }
     }
     return arr.length === 0 ? 1 : 0;
 }
 
-console.log(solution(s));
+console.log(solution(s)); 
