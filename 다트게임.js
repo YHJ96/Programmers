11e0c05285b3c47a295383a08323dc143b8b8787
YHJ96 <yhj878812@naver.com>
2021-10-03 16:19:00 +0900

    YHJ96

--- 다트게임.js
+++ 다트게임.js
@@ -2,8 +2,11 @@ const s = '1D#2S*3S';
 
 console.log(solution(s));
 
+// 해결중
 function solution(s) {
     const arr = [];
+    const result = [];
+    let answer = [];
     let flag = '';
     for(let x of s) {
         if(x === 'S' || x === 'D' || x === 'T') {
@@ -13,5 +16,23 @@ function solution(s) {
             arr.push(x);
         } else flag += x;
     }
-    return arr;
+    for(let x of arr) {
+        if(x.includes('S')) result.push(Number(x.split('S')[0])); 
+        else if(x.includes('D')) result.push(Number(x.split('D')[0]) ** 2);
+        else if(x.includes('T')) result.push(Number(x.split('T')[0]) ** 3);
+        else result.push(x);
+    }
+    const resultStar = result.filter((item) => item !== '#');
+    console.log(resultStar);
+    for(let i = 0; i < resultStar.length; i++) {
+        let count = 0;
+        for(let j = i; j !== 0; j--) {
+            if(count === 2) break;
+            if(typeof resultStar[j] === 'number' && resultStar[i] === '*') {
+                count += 1;
+                resultStar[j] = resultStar[j] * 2;
+            };
+        }
+    }
+    return resultStar;
 }
