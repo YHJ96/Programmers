1c74757bf32998425474c0bd2977180f215a6cbf
YHJ96 <yhj878812@naver.com>
2021-10-10 16:21:54 +0900

    YHJ96

--- 소수만들기.js
+++ 소수만들기.js
@@ -5,6 +5,7 @@ console.log(solution(nums));
 function solution(nums) {
     let answer = 0;
     let result = [];
+    // 완전탐색
     for (let i = 0; i < nums.length - 2; i++) {
         for (let j = i + 1; j < nums.length - 1; j++) {
             for (let k = j + 1; k < nums.length; k++) {
@@ -12,7 +13,9 @@ function solution(nums) {
             }
         }
     }
+    // 최대범위 구간
     const max = Math.max(...result);
+    // 에라토스테네스의 체 사용
     for (let k = 0; k <= result.length; k++) {
         for (let i = 2; i <= max; i++) {
             for (let j = i + i; j <= max; j += i) {
@@ -20,6 +23,7 @@ function solution(nums) {
             }
         }
     }
+    // 0이 아닌숫자 소수의 개수
     for(let x of result) {
         if(x !== 0) answer += 1;
     }
