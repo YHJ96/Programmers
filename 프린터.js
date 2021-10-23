12778d4ab6f4aad68fc0d0f0603daf4fc54eb4e6
YHJ96 <yhj878812@naver.com>
2021-10-23 16:43:49 +0900

    해결완료

--- 프린터.js
+++ 프린터.js
@@ -1,21 +1,37 @@
-const priorities = [4, 2, 1, 3, 2, 5];
-const location = 2;
-
-console.log(solution(priorities, location))
+const priorities = [1, 1, 9, 1, 1, 1];
+const location = 0;
 
 function solution(priorities, location) {
-    let item = priorities[location];
     let answer = 0;
-    const comapre = [];
-    for(let x of priorities) {
-        if(x > item) answer += 1;
-        else comapre.push(x);
+    while(true) {
+        // 배열의 우선순위 최대값 구하기
+        const max = Math.max(...priorities);
+        // 배열의 0 인자가 우선도가 제일 높고 구하고자 하는 위치의 종이와 일치하면 함수종료
+        if(priorities[0] === max && location === 0) {
+            answer += 1;
+            break;
+            // 배열의 0 인자가 우선도가 제일 높지 않고 구하고자 하는 위치의 종이와 일치할 경우
+        } else if(priorities[0] !== max && location === 0) {
+            // 첫번 째 인자를 빼주고 다시 해당 배열뒤에 넣기
+            const value = priorities.shift();
+            priorities.push(value);
+            // 위치 마지막 인덱스로 초기화
+            location = priorities.length - 1;
+            // 배열의 0인자가 우선도만 제일 높았을 경우
+        } else if(priorities[0] === max) {
+            // 첫번 쨰 인자를 빼주고 위치를 한칸씩 당겨줌
+            priorities.shift();
+            location -= 1;
+            answer += 1;
+            // 배열의 0인자가 우선도가 높지 않을 경우
+        } else if(priorities[0] !== max) {
+            // 배열의 인자를 빼주고 다시 해당 배열뒤에 넣고 위치 당겨줌
+            const value = priorities.shift();
+            priorities.push(value);
+            location -= 1;
+        }
     }
-    const max = Math.max(...comapre);
-    let count = 0;
-    for(let x of comapre) {
-        if(x === max) count += 1;
-    }
-    if(count === 1) return answer + 1;
-    else return answer + location + 1;
+    return answer; 
 }
+
+console.log(solution(priorities, location));
