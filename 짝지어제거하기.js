75a25ae2500dfe555f254eac542eef1af9e2381a
YHJ96 <yhj878812@naver.com>
2021-10-26 17:51:24 +0900

    해결완료

--- 짝지어제거하기.js
+++ 짝지어제거하기.js
@@ -1,20 +1,17 @@
 const s = 'baabaa';
 
-// 푸는중
-// 테스트케이스 실패
 function solution(s) {
-    let count = 0;
-    let arr = [...s];
-    while(count !== arr.length) {
-        count = arr.length;
-        for (let i = 0; i < arr.length - 1; i++) {
-            if (arr[i] === arr[i + 1]) {
-                arr.splice(i + 1, 1);
-                arr.splice(i, 1);
-            }
-        }
+    let answer = 1;
+    // stack으로 해결
+    const stack = [];
+    for (let i = 0; i < s.length; i++) {
+        // stack에 인자가 없으면 넣고 stack의 마지막 원소와 s[i]가 같으면 빼기
+        if(stack.length === 0 || stack[stack.length - 1] !== s[i]) stack.push(s[i]);
+        // s[i]인자와 스택 마지막인자가 같다면 pop
+        else stack.pop();
     }
-    return arr.length === 0 ? 1 : 0;
+    if(stack.length !== 0) answer = 0;
+    return answer;
 }
 
-console.log(solution(s)); 
+console.log(solution(s));
