2cd528b2f2ff79b66ed5d2d820479dbf18e5d714
YHJ96 <yhj878812@naver.com>
2021-11-01 14:21:48 +0900

    해결완료

--- 최솟값만들기.js
+++ 최솟값만들기.js
@@ -1,8 +1,10 @@
 function solution(A,B) {
+    // 최솟값을 만드는 방법은 A의 큰수와 B의 작은수부터 곱해나가는 경우
     A.sort((a,b) => a-b);
     B.sort((a,b) => b-a);
     let answer = 0;
     for(let i = 0; i < A.length; i++) {
+        // 오름차순 정렬인 A의 인자와 내림차순 정렬인 B의 인자와 곱하기
        answer += (A[i] * B[i]); 
     }
     return answer;
