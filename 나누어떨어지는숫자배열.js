9fd732b2218f02a14e3a33311cf92c17da3e67cf
YHJ96 <yhj878812@naver.com>
2021-11-21 18:21:08 +0900

    해결완료

--- 나누어떨어지는숫자배열.js
+++ 나누어떨어지는숫자배열.js
@@ -2,6 +2,7 @@ const arr = [5,9,7,10];
 const divisor = 5;
 
 function solution(arr, divisor) {
+    // 나머지값이 0 인수 배열에서 찾기
     let answer = arr.filter((item) => item % divisor === 0);
     answer = answer.sort((a,b) => a - b);
     if(answer.length === 0 ) return [-1];
