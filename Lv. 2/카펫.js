7df8bd458cee7145f72af8c4b4542330af8199ec
YHJ96 <yhj878812@naver.com>
2022-04-05 17:17:48 +0900

    해결완료

--- Lv. 2/카펫.js
+++ Lv. 2/카펫.js
@@ -1,18 +1,32 @@
 const brown = 10;
 const yellow = 2;
 
-console.log(solution(brown, yellow));
-
 function solution(brown, yellow) {
-    const sum = brown + yellow;
-    let count = sum - 1;
-    let col;
-    while(true) {
-        if(sum % count === 0) {
-            col = sum / count;
-        }
-        if(col % 2) break;
-        count--;
-    } 
-    return [count, sum / count];
+    let answer = [];
+    // 전체의 좌표 개수를 구한다.
+    const all = brown + yellow;
+    // 약수들을 넣을 변수 초기화
+    const divisor = [];
+    // 전체 좌표의 개수부터 1 까지 반복문 실행 
+    for(let i = all; i >= 1; i--) {
+        // 만약 전체 개수의 i의 약수면 조건문 실행
+        if(all % i === 0) {
+            // i로 나눈 몫
+            const num = all / i;
+            // 몫이 i 보다 크거나 같으면 divisor에 push
+            if(num <= i) divisor.push(i);
+        };
+    }
+    // divesor의 요소들을 탐색
+    for(let x of divisor) {
+        // y 좌표를 초기화
+        const y = all / x;
+        // x 좌표 + y 좌표의 전체 외곽의 수를 더해주고 꼭지점 4개를 빼준다. ( 중복의 이유 )
+        const count = (2 * y) + (2 * x) - 4;
+        // count과 brown과 같으면 answer에 좌표 push
+        if(count === brown) answer.push(x, y);
+    }
+    return answer;
 }
+
+console.log(solution(brown, yellow));
